8a883faf6e07f75895d68389f1d3f615a8f4826d
am ir <amirmohammadshi@hotmail.com>
2025-03-07 23:26:29 +0330

    make users controller

---
+++ ResFin.WebApi/Controllers/Users/RegisterUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using ResFin.Domain.Shared;
+using ResFin.Domain.Users;
+
+namespace ResFin.WebApi.Controllers.Users;
+
+public sealed record RegisterUserRequest (
+    string FirstName,
+    string LastName,
+   string Email,
+    string Phone,
+    string CellPhone,
+    Address Address,
+    UserType UserType,
+    string Password
+    );
---
+++ ResFin.WebApi/Controllers/Users/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResFin.Application.Users.RegisterUser;
+
+namespace ResFin.WebApi.Controllers.Users
+    {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+
+    public class UsersController : ControllerBase
+        {
+        private readonly ISender _sender;
+
+        public UsersController ( ISender sender )
+            {
+            _sender = sender;
+            }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Register (
+            RegisterUserRequest request,
+            CancellationToken cancellationToken )
+            {
+            var command = new RegisterUserCommand(
+                request.FirstName,
+                request.LastName,
+                request.Email,
+                request.Phone,
+                request.CellPhone,
+                request.Address,
+                request.UserType,
+                request.Password
+                );
+
+            var result = await _sender.Send(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+
+            return Ok(result.Value);
+            }
+        }
+    }
